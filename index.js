f6c439e4cc0440c9b91ab716f31b4d183ac24582
Tiqur <Tiqur7@gmail.com>
2021-10-16 01:19:37 -0700

    Fetch vocab step links

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ puppeteer.use(StealthPlugin());
 (async () => {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
+
   console.log("Connecting to website..")
   await page.goto('https://iknow.jp/content/japanese');
-  console.log("Done!")
+
+  // Fetch each vocab word step link ( remove first two since they deal with kana, not vocab )
+  const handles = (await page.$$eval('.shiv-border-box-sizing > li > div > a', anchors => [].map.call(anchors, a => a.href))).slice(2);
+  console.log(handles.length);
+
+  console.log("Done!");
 })();
